e8bded60b9d020acfe2d3ae020ce6b037cd79804
Tri M. Nguyen <mail@trimn.net>
2014-05-11 03:22:58 +0200

    Add functionality to add bookmarks to bookmark view

--- scripts/manager.js
+++ scripts/manager.js
@@ -1,16 +1,60 @@
 OB = window.OB || {};
 
 (function () {
+  var active_folder = '1';
+
+  var fill_bookmarks_view = function (bookmarks) {
+    var bookmark_list = document.querySelector('#right-content div ul');
+    bookmark_list.innerHTML = '';
+
+    for (var i in bookmarks) {
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      var img = document.createElement('img');
+
+      img.setAttribute('src', Browser.info.vendor + '://favicon/' + bookmarks[i].url);
+      a.setAttribute('href', bookmarks[i].url);
+      a.appendChild(document.createTextNode(bookmarks[i].title));
+
+      li.appendChild(img);
+      li.appendChild(a);
+
+      bookmark_list.appendChild(li);
+    }
+  };
+
+  var folder_click = function (e) {
+    active_folder = e.target.dataset.id + '';
+    chrome.bookmarks.getChildren(e.target.dataset.id, function (children) {
+      fill_bookmarks_view(children);
+    });
+  };
+
   OB.get_folders(OB.section.MANAGER, function (tree) {
     for (var t in tree) {
-      var doc = document.createDocumentFragment();
-      var li = document.createElement('li');
-      li.setAttribute('data-id', tree[t].id);
-      li.setAttribute('id', 'folder_' + tree[t].id);
+      var li   = document.createElement('li')
+      , button = document.createElement('button');
 
-      li.innerHTML = '<button class="btn btn-folder-bar">' + tree[t].title + '</button>';
+      button.setAttribute('data-index', tree[t].index);
+      button.setAttribute('data-id', tree[t].id);
+      button.setAttribute('id', 'folder_' + tree[t].id);
+      button.setAttribute('class', 'btn btn-folder-bar');
+
+      button.appendChild(document.createTextNode(tree[t].title));
+
+      li.appendChild(button);
+      li.addEventListener('click', folder_click);
 
       document.querySelector('#left-content ul').appendChild(li);
     }
   });
+
+
+  /* INIT
+  ----------------------------------------------------------------------------*/
+  chrome.bookmarks.getChildren(active_folder, function (children) {
+    fill_bookmarks_view(children);
+  });
+
+
 })();
